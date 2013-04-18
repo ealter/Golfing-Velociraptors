03c35cda2c89424cf0cc277dccfe7180df41fac7
Eliot Alter <Eliot.Alter@gmail.com>
2013-04-17 21:26:41 -0400

    Add the data set to the viz state

--- eclipse/src/edu/tufts/cs/gv/controller/VizState.java
+++ eclipse/src/edu/tufts/cs/gv/controller/VizState.java
@@ -3,11 +3,22 @@ package edu.tufts.cs.gv.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.tufts.cs.gv.model.Dataset;
+
 public class VizState {
 	private List<VizUpdateListener> updateListeners = new ArrayList<>();
 	
 	private static VizState singletonVizState;
+	private Dataset dataset;
 	
+	public Dataset getDataset() {
+		return dataset;
+	}
+
+	public void setDataset(Dataset dataset) {
+		this.dataset = dataset;
+	}
+
 	public static VizState getState() {
 		if(singletonVizState == null) {
 			singletonVizState = new VizState();
